4f43298175a6a283314a0e11527199ef36b57fd9
EniseB <enise-j-bernards@web.de>
2023-03-15 13:31:22 +1300

    optimise code by destructuring

--- monsters-rolodex-ztm/src/App.js
+++ monsters-rolodex-ztm/src/App.js
@@ -29,8 +29,11 @@ class App extends Component {
 
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
     })
 
     return (
@@ -39,7 +42,7 @@ class App extends Component {
           className="searchBox"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
 
         {filteredMonsters.map((monster) => {
