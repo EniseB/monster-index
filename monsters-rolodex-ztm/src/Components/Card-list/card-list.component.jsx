2765baae1d71746852441817395b374b22366c74
EniseB <enise-j-bernards@web.de>
2023-03-15 14:02:24 +1300

    create component folder & first component file

---
+++ monsters-rolodex-ztm/src/Components/Card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Something</p>
+            </div>
+        )
+    }
+}
+
+export default CardList;
